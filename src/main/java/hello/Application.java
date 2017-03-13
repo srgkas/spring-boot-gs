5ccbb4cf0124b1f3f97137ccef23cf012690f9b7
Serhii Kasianchuk <srg.kas@pdffiller.com>
2017-03-13 18:18:25 +0200

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,71 @@
+package hello;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    final static String queueName = "spring-boot";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("spring-boot-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/")
+    public HashMap sendMessage(@RequestParam(value = "msg", defaultValue = "Hello from RabbitMQ!") String message) {
+        ConnectionFactory connectionFactory = new CachingConnectionFactory();
+        AmqpAdmin admin = new RabbitAdmin(connectionFactory);
+        admin.declareQueue(queue());
+        AmqpTemplate template = new RabbitTemplate(connectionFactory);
+        template.convertAndSend(Application.queueName, message);
+        HashMap<String, String> result = new HashMap<String, String>();
+        result.put("result", "Message sent");
+
+        return result;
+    }
+}
